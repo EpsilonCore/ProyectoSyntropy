64ba821f14c2c4d643f0d9f4d4100d7e613a9d00
Kevshos <kcarballo625@gmail.com>
2026-08-19 18:29:47 -0300

    Frontend para incidencias

--- Gestion/IncidenciaController.php
+++ Gestion/IncidenciaController.php
@@ -28,7 +28,6 @@ class IncidenciaController{
             if(!$datos|| !isset($datos->mail) || !isset($datos->tipo) || !isset($datos->estado) || !isset($datos->imagen) || !isset($datos->calle) || !isset($datos->numero) || !isset($datos->barrio)){
                 http_response_code(400);
                 return ['status' => 'error', 'mensaje' => 'Faltan campos obligatorios'];
-                exit;
             }
             $m = $datos->mail;
             $t = $datos->tipo;
@@ -37,7 +36,7 @@ class IncidenciaController{
             $c = $datos->calle;
             $n = $datos->numero;
             $b = $datos->barrio;
-
+ 
         $resultado= $this->modeloObj -> crearIncidencia($m, $t, $e, $i, $c,$n,$b);
 
         if ($resultado) {
@@ -50,6 +49,34 @@ class IncidenciaController{
     public function AsignarOperario(){
         $json = file_get_contents('php://input');
         $datos = json_decode($json);
+        if(!$datos || !isset($datos->idIncidencia) || !isset($datos->idOperario)){
+            http_response_code(400);
+            return ['status' => 'error', 'mensaje' => 'Faltan campos obligatorios'];
+        } else{
+            
+            $resultado = $this->modeloObj->AsignarOperario($datos->idIncidencia, $datos->idOperario);
+            if ($resultado) {
+                return ["status" => "success", "mensaje" => "Operario asignado correctamente"];
+            } else {
+                return ["status" => "error", "mensaje" => "No se pudo asignar el operario"];
+            }
+        }
         
+    }
+
+    public function eliminarIncidencia(){
+        $json = file_get_contents('php://input');
+        $datos = json_decode($json);
+        if(!$datos || !isset($datos->idIncidencia)){
+            http_response_code(400);
+            return ['status' => 'error', 'mensaje' => 'Faltan campos obligatorios'];
+        } else{
+            $resultado = $this->modeloObj->eliminarIncidencia($datos->idIncidencia);
+            if ($resultado) {
+                return ["status" => "success", "mensaje" => "Incidencia eliminada correctamente"];
+            } else {
+                return ["status" => "error", "mensaje" => "No se pudo eliminar la incidencia"];
+            }
+        }
     }
 }
